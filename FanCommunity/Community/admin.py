b7388b3820bf9fa10b2d1948ea758caa8e4c2e2e
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-23 23:48:03 +0300

    "Register models in admin panel for management and testing"

--- FanCommunity/Community/admin.py
+++ FanCommunity/Community/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Movie, FootballTeam, Post, Comment, Like, FavoriteTeam, FavoriteMovie
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Movie)
+admin.site.register(FootballTeam)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(FavoriteTeam)
+admin.site.register(FavoriteMovie)
