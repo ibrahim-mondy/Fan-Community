bae926efc315f364776d337c88ba01427ad3a929
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-31 03:54:32 +0300

    update

--- FanCommunity/Community/urls.py
+++ FanCommunity/Community/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    MovieViewSet,
-    FootballTeamViewSet,
-    PostViewSet,
-    CommentViewSet,
-    LikeViewSet,
-    FavoriteMovieViewSet,
-    FavoriteTeamViewSet
+    UserViewSet, MovieViewSet, FootballTeamViewSet,
+    PostViewSet, CommentViewSet,
+    LikeViewSet, FavoriteMovieViewSet, FavoriteTeamViewSet,
+    signup, login, logout
 )
 
 router = DefaultRouter()
@@ -17,12 +14,12 @@ router.register(r'teams', FootballTeamViewSet, basename='footballteam')
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 router.register(r'likes', LikeViewSet, basename='like')
-router.register(r'favorite-teams', FavoriteTeamViewSet, basename='favoriteteam')
-router.register(r'favorite-movies', FavoriteMovieViewSet, basename='favoritemovie')
+router.register(r'favorite-movies', FavoriteMovieViewSet, basename='favorite-movie')
+router.register(r'favorite-teams', FavoriteTeamViewSet, basename='favorite-team')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
+    path('auth/signup/', signup, name='signup'),
+    path('auth/login/', login, name='login'),
+    path('auth/logout/', logout, name='logout'),
 ]
