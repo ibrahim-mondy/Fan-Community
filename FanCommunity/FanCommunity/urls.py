12ad065c40795c58c558e115855b0144b5d852a9
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-31 03:49:46 +0300

    fixed

--- FanCommunity/FanCommunity/urls.py
+++ FanCommunity/FanCommunity/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('authentication.urls')),
+    path('api/auth/', include('rest_framework.urls')),
     path('api/', include('Community.urls')),  
     ]
