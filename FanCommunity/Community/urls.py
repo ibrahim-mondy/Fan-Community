34bc3245c158876baa2ee7af196e5c17f6ecc875
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-31 03:45:21 +0300

    update

--- FanCommunity/Community/urls.py
+++ FanCommunity/Community/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
 from .views import (
     UserViewSet, MovieViewSet, FootballTeamViewSet,
-    PostViewSet, CommentViewSet,
+    PostViewSet, CommentViewSet, LikeViewSet,
+    FavoriteTeamViewSet, FavoriteMovieViewSet,
     signup, login, logout
 )
 
@@ -14,20 +13,13 @@ router.register(r'movies', MovieViewSet, basename='movie')
 router.register(r'teams', FootballTeamViewSet, basename='footballteam')
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'likes', LikeViewSet, basename='like')
+router.register(r'favorite-teams', FavoriteTeamViewSet, basename='favoriteteam')
+router.register(r'favorite-movies', FavoriteMovieViewSet, basename='favoritemovie')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Authentication routes
-    path('api/auth/', include('authentication.urls')),
-
-    # Core app routes
-    path('api/movies/', include('movies.urls')),
-    path('api/football-teams/', include('football.urls')),
-    path('api/posts/', include('posts.urls')),
-    path('api/comments/', include('comments.urls')),
-    path('api/likes/', include('likes.urls')),
-    path('api/favorite-teams/', include('favorites.urls_team')),
-    path('api/favorite-movies/', include('favorites.urls_movie')),
+    path('', include(router.urls)),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
-
